ed35c2b7b798f5e41308fe6d738032f209eb4ab0
Pablo Lacerda de Miranda <placerdademiranda@netflix.com>
2020-05-30 23:22:46 -0700

    adds more logic to the parse to scan simple language characters and operators

--- src/lib/__tests__/scanner.test.ts
+++ src/lib/__tests__/scanner.test.ts
@@ -1,4 +1,6 @@
 import Scanner from '../scanner';
+import { TokenType } from '../TokenType';
+import Token from '../Token';
 
 test('initialize the scanner', () => {
   const source = '';
@@ -6,8 +8,33 @@ test('initialize the scanner', () => {
   expect(scanner).toBeInstanceOf(Scanner);
 });
 
-test('can retrieve the list of tokens', () => {
+test('retrieves the list of tokens', () => {
   const source = '';
+  const EOFToken = new Token(TokenType.EOF, null, null, 1);
   const scanner = new Scanner(source);
-  expect(scanner.scanTokens()).toStrictEqual([]);
+  expect(scanner.scanTokens()).toStrictEqual([EOFToken]);
+});
+
+test('scans special characters', () => {
+  const source = `{}().-+;*`;
+  const scanner = new Scanner(source);
+  const tokens = scanner.scanTokens();
+  // the returned tokens will include a EOF token as the last token
+  expect(tokens.length).toBe(source.length + 1);
+});
+
+test('scans operators', () => {
+  const source = `! != = < <= > >= == /`;
+  const scanner = new Scanner(source);
+  const tokens = scanner.scanTokens();
+  // All operators are separated by empty space, adds 1 to represent the EOF token
+  expect(tokens.length).toBe(source.split(' ').length + 1);
+});
+
+test('ignores comment lines', () => {
+  const source = `// this is a comment line`;
+  const scanner = new Scanner(source);
+  const tokens = scanner.scanTokens();
+  const EOFToken = new Token(TokenType.EOF, null, null, 1);
+  expect(tokens).toStrictEqual([EOFToken]);
 });
--- src/lib/scanner.ts
+++ src/lib/scanner.ts
@@ -1,4 +1,5 @@
 import Token from './Token';
+import { TokenType } from './TokenType';
 
 class Scanner {
   tokens: Token[];
@@ -9,27 +10,119 @@ class Scanner {
     this.tokens = [];
     this.start = 0;
     this.current = 0;
-    this.line = 0;
+    this.line = 1;
   }
 
   scanTokens() {
-    const tokens = [];
-    let start = 0;
-    let current = 0;
-    let line = 1;
-    while (!this.isAtEnd(current)) {
-      start = current;
+    while (!this.isAtEnd()) {
+      this.start = this.current;
       this.scanToken();
     }
-    return tokens;
+
+    this.tokens.push(new Token(TokenType.EOF, null, null, this.line));
+    return this.tokens;
   }
 
-  isAtEnd(current: number) {
-    return current >= this.source.length;
+  isAtEnd() {
+    return this.current >= this.source.length;
   }
 
   scanToken() {
+    const c = this.advance();
+    switch (c) {
+      case TokenType.LEFT_PAREN:
+        this.addToken(TokenType.LEFT_PAREN);
+        break;
+      case TokenType.RIGHT_PAREN:
+        this.addToken(TokenType.RIGHT_PAREN);
+        break;
+      case TokenType.LEFT_BRACE:
+        this.addToken(TokenType.LEFT_BRACE);
+        break;
+      case TokenType.RIGHT_BRACE:
+        this.addToken(TokenType.RIGHT_BRACE);
+        break;
+      case TokenType.COMMA:
+        this.addToken(TokenType.COMMA);
+        break;
+      case TokenType.DOT:
+        this.addToken(TokenType.DOT);
+        break;
+      case TokenType.MINUS:
+        this.addToken(TokenType.MINUS);
+        break;
+      case TokenType.PLUS:
+        this.addToken(TokenType.PLUS);
+        break;
+      case TokenType.SEMICOLON:
+        this.addToken(TokenType.SEMICOLON);
+        break;
+      case TokenType.STAR:
+        this.addToken(TokenType.STAR);
+        break;
+
+      // Operators
+      case TokenType.BANG:
+        this.addToken(
+          this.match(TokenType.EQUAL) ? TokenType.BANG_EQUAL : TokenType.BANG
+        );
+        break;
+      case TokenType.EQUAL:
+        this.addToken(
+          this.match(TokenType.EQUAL) ? TokenType.EQUAL_EQUAL : TokenType.EQUAL
+        );
+        break;
+      case TokenType.LESS:
+        this.addToken(
+          this.match(TokenType.EQUAL) ? TokenType.LESS_EQUAL : TokenType.LESS
+        );
+        break;
+      case TokenType.GREATER:
+        this.addToken(
+          this.match(TokenType.EQUAL)
+            ? TokenType.GREATER_EQUAL
+            : TokenType.GREATER
+        );
+        break;
+      case TokenType.SLASH:
+        if (this.match(TokenType.SLASH)) {
+          // this is a comment, we need to consume all the characters
+          // until the end of line
+          while (this.peek() !== '\n' && this.isAtEnd()) {
+            this.advance();
+          }
+          break;
+        } else {
+          this.addToken(TokenType.SLASH);
+          break;
+        }
+    }
+  }
+
+  /**
+   * Take a look on the current character
+   */
+  peek() {
+    if (this.isAtEnd()) return '\0';
+    return this.source[this.current];
+  }
+
+  match(expected: string) {
+    if (this.isAtEnd()) return false;
+    if (this.source[this.current] !== expected) return false;
+
     this.current++;
+    return true;
+  }
+
+  advance() {
+    this.current++;
+    return this.source[this.current - 1];
+  }
+
+  addToken(tokenType: TokenType, literal?: object) {
+    const text = this.source.substring(this.start, this.current);
+    this.tokens.push(new Token(tokenType, text, literal, this.line));
   }
 }
 
